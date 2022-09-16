6227189fcf57bfde0806b9d9dc31525e58e343ae
Aleksey <pilot-1981@yandex.ru>
2022-09-16 14:59:08 +0500

    add DZ_6_Task_1.2

---
+++ DZ_6/Task_1.2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите элементы массива (через пробел): ");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = 0;
+
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > 0)
+    {
+        count++;
+    }
+}
+
+Console.WriteLine($"Количество чисел > 0: {count}");
