9e6cbfb58f0ba6429ab595cdd793b9f27cfbfe11
Aleksey <pilot-1981@yandex.ru>
2022-09-16 10:39:42 +0500

    add Sem_6_Task_2.1

---
+++ Sem_6/Task_2.1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите десятичное число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int[] array = new int [100];
+int remainder;
+string result = string.Empty;
+while (N > 0)
+{
+    remainder = N % 2;
+    N = N / 2;
+    result = remainder.ToString() + result;
+    Console.WriteLine (result);
+}
+Console.WriteLine("Двоичное {0} ", result);
