f3d567e5045ccd69a82ea30b8364912839c01040
Aleksey <pilot-1981@yandex.ru>
2022-09-16 10:38:59 +0500

    add Sem_6_Task_2

--- Sem_6/Task_1/Program.cs
+++ Sem_6/Task_1/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] array = new int[] { 1, 2, 3, 4, 5 }; // по идее должен создать массив с 5 элементами array [1, 2, 3, 4, 5]
+for (int i = 0; i < array.Length / 2; i++)
+{
+    int tmp = array[i];
+    array[i] = array[array.Length - (i + 1)];
+    array[array.Length - (i + 1)] = tmp;
+}
+Console.WriteLine(" E_N_D ");
---
+++ Sem_6/Task_2/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int[] arr = new int[100];
+int i = 0;
+while (N > 0)
+{
+    arr[i] = N % 2;
+    Console.Write(arr[i]);
+    N = N / 2;
+    i++;
+}
+Console.WriteLine();
+Console.WriteLine("i = " + i);
+
+N = arr[i];
+Console.WriteLine("-N- = " + N);
+Console.WriteLine("arr[i] = " + arr[i]);
+while (i > -1)
+{
+    N = N * 10 + arr[i];
+    Console.WriteLine("N = " + N);
+    i--;
+}
+Console.WriteLine(N);
